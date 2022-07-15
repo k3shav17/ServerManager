158fd5fe560d7333647c73bab4bba48fca5914dd
k3shav17 <keshavrao0489@gmail.com>
2022-07-15 18:12:47 +0530

    added functionality in app component to comm with service

--- server-man/src/app/app.component.ts
+++ server-man/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { DataState } from './enum/data-state.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'server-man';
+export class AppComponent implements OnInit {
+  appState$: Observable<AppState<CustomResponse>>;
+
+  constructor(private serverService: ServerService) {}
+
+  ngOnInit() {
+    this.appState$ = this.serverService.server$.pipe(
+      map((response) => {
+        return { dataState: DataState.LOADED, appData: response };
+      }),
+      startWith({ dataState: DataState.LOADING }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR, error });
+      })
+    );
+  }
 }
